d85901340cc995e900a48f1c5d2df23a4ad8c0ea
Chris Rimmer <c@24.org.uk>
2013-06-23 00:26:35 +0100

    Added ordinal inflection for integers

--- src/main/scala/uk/me/chrs/inflect/Inflect.scala
+++ src/main/scala/uk/me/chrs/inflect/Inflect.scala
@@ -3,7 +3,21 @@ package uk.me.chrs.inflect
 object Inflect {
 
   def ordinal (number: Int) = {
-    "" + number + "th"
+
+    def digitSuffix(digit: Int): String = {
+      digit match {
+        case 1 => "st"
+        case 2 => "nd"
+        case 3 => "rd"
+        case _ => "th"
+      }
+    }
+
+    def ordinalSuffix(number: Int): String = {
+      if (number % 100 /10 == 1) "th" else digitSuffix(number % 10)
+    }
+
+    number + ordinalSuffix(number)
   }
 
 }
--- src/test/scala/uk/me/chrs/inflect/InflectTest.scala
+++ src/test/scala/uk/me/chrs/inflect/InflectTest.scala
@@ -8,4 +8,33 @@ class InflectTest extends FunSuite {
     assert(Inflect.ordinal(7) === "7th")
   }
 
+  test("1 becomes 1st") {
+    assert(Inflect.ordinal(1) === "1st")
+  }
+
+  test("2 becomes 2nd") {
+    assert(Inflect.ordinal(2) === "2nd")
+  }
+
+  test("3 becomes 3rd") {
+    assert(Inflect.ordinal(3) === "3rd")
+  }
+
+  test("101 becomes 101st") {
+    assert(Inflect.ordinal(101) === "101st")
+  }
+
+  test("11 becomes 11th") {
+    assert(Inflect.ordinal(11) === "11th")
+  }
+
+  test("62 becomes 62nd") {
+    assert(Inflect.ordinal(62) === "62nd")
+  }
+
+  test("312 becomes 312th") {
+    assert(Inflect.ordinal(312) === "312th")
+  }
+
+
 }
