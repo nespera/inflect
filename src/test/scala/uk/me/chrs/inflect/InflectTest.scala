e5abc3887927d3a96ca513a5dcd0490daf8bcc75
Chris Rimmer <c@24.org.uk>
2013-06-23 00:39:40 +0100

    Added DCONWAY's tests

--- src/test/scala/uk/me/chrs/inflect/InflectTest.scala
+++ src/test/scala/uk/me/chrs/inflect/InflectTest.scala
@@ -1,40 +1,44 @@
 package uk.me.chrs.inflect
 
 import org.scalatest.FunSuite
+import uk.me.chrs.inflect.Inflect._
 
 class InflectTest extends FunSuite {
 
-  test("th gets added to most numbers") {
-    assert(Inflect.ordinal(7) === "7th")
+  test("DCONWAY's tests plus some of mine") {
+    assert(ordinal(0) === "0th")
+    assert(ordinal(1) === "1st")
+    assert(ordinal(2) === "2nd")
+    assert(ordinal(3) === "3rd")
+    assert(ordinal(4) === "4th")
+    assert(ordinal(5) === "5th")
+    assert(ordinal(6) === "6th")
+    assert(ordinal(7) === "7th")
+    assert(ordinal(8) === "8th")
+    assert(ordinal(9) === "9th")
+    assert(ordinal(10) === "10th")
+    assert(ordinal(11) === "11th")
+    assert(ordinal(12) === "12th")
+    assert(ordinal(13) === "13th")
+    assert(ordinal(14) === "14th")
+    assert(ordinal(15) === "15th")
+    assert(ordinal(16) === "16th")
+    assert(ordinal(17) === "17th")
+    assert(ordinal(18) === "18th")
+    assert(ordinal(19) === "19th")
+    assert(ordinal(20) === "20th")
+    assert(ordinal(21) === "21st")
+    assert(ordinal(22) === "22nd")
+    assert(ordinal(23) === "23rd")
+    assert(ordinal(24) === "24th")
+    assert(ordinal(62) === "62nd")
+    assert(ordinal(100) === "100th")
+    assert(ordinal(101) === "101st")
+    assert(ordinal(102) === "102nd")
+    assert(ordinal(103) === "103rd")
+    assert(ordinal(104) === "104th")
+    assert(ordinal(312) === "312th")
+    assert(ordinal(8014) === "8014th")
+    assert(ordinal(10004) === "10004th")
   }
-
-  test("1 becomes 1st") {
-    assert(Inflect.ordinal(1) === "1st")
-  }
-
-  test("2 becomes 2nd") {
-    assert(Inflect.ordinal(2) === "2nd")
-  }
-
-  test("3 becomes 3rd") {
-    assert(Inflect.ordinal(3) === "3rd")
-  }
-
-  test("101 becomes 101st") {
-    assert(Inflect.ordinal(101) === "101st")
-  }
-
-  test("11 becomes 11th") {
-    assert(Inflect.ordinal(11) === "11th")
-  }
-
-  test("62 becomes 62nd") {
-    assert(Inflect.ordinal(62) === "62nd")
-  }
-
-  test("312 becomes 312th") {
-    assert(Inflect.ordinal(312) === "312th")
-  }
-
-
 }
